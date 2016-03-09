1c92670dc1c97eb25c945f611403bbedb8cabf9a
trainboy2019 <ikerolader@gmail.com>
2016-03-09 17:05:53 -0500

    d

---
+++ adRemoval.js
@@ -0,0 +1,31 @@
+if (window.navigator.userAgent.indexOf("Macintosh; Intel Mac OS X")){
+	document.getElementById("19").write("OSName2"
+	$("#ad").animate{
+		'opacity': '0'
+	}
+	document.write("OSName");
+}
+else if (window.navigator.userAgent.indexOf("iPhone")) {
+	document.getElementById("ad").src="";
+	document.write("OSName1");
+	$("#ad").animate{
+		'opacity': '0'
+	}
+}
+else if (window.navigator.userAgent.indexOf("iPod")) {
+	document.getElementById("ad").src="";
+	document.getElementById("19").write("OSName2")
+	$("#ad").animate{
+		'opacity': '0'
+	}
+}
+else if (window.navigator.userAgent.indexOf("iPad")) {
+	document.getElementById("ad").src="";
+	document.getElementById("19").write("OSName2"
+	$("#ad").animate{
+		'opacity': '0'
+	}
+}
+else{
+	
+}
