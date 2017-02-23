15a00dc730a1be8fc79ebf1750637c5db1c26d34
trainboy2019 <ikerolader@gmail.com>
2017-02-22 20:06:33 -0500

    Google Test

--- GetPost/welcome.php
+++ GetPost/welcome.php
@@ -1,8 +1,33 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
 
+Your Gravitar image is:<img src="https://www.gravatar.com/avatar/<?php $mail = md5( strtolower( trim( $_POST["email"] ) ) ); echo $mail;?>">
+<?php $profileurl = "https://www.gravatar.com/".$mail.".php"; echo $profileurl;?>
+<br />
+<?php
+
+ function curl_get_contents($url)
+{
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
+
+$str = curl_get_contents( profileurl );
+$profile = unserialize( $str );
+if ( is_array( $profile ) && isset( $profile['entry'] ) )
+    echo $profile['entry'][0]['displayName'];
+echo $str;
+ // echo "lol";
+?>
+<?php echo $profile;?>
 </body>
 </html>
