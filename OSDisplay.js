2c15fdb76fc291c5bb07fb03a99c49ec24ba0ce7
trainboy2019 <ikerolader@gmail.com>
2017-04-10 08:24:25 -0400

    Nintendo Switch Support + WIP Modified Chrome T-Rex

--- OSDisplay.js
+++ OSDisplay.js
@@ -9654,6 +9654,8 @@ if (window.navigator.userAgent.indexOf("iPhone; CPU iPhone OS "+iOS1+"."+iOS2+".
 
 		if (window.navigator.userAgent.indexOf("Nintendo WiiU") != -1) OSName="Nintendo Wii U";
 
+		if (window.navigator.userAgent.indexOf("Nintendo Switch") != -1) OSName="Nintendo Switch";
+
 		if (window.navigator.userAgent.indexOf("PlayStation 4") != -1) OSName="PlayStation 4";
 
 		if (window.navigator.userAgent.indexOf("PLAYSTATION 3") != -1) OSName="PlayStation 3";
